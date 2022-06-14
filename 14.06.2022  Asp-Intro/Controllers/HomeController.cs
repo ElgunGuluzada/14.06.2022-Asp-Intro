99dc49645819e3640e4f192bd8b15d99039a7518
Elgun Guluzada <elgunpg@code.edu.az>
2022-06-15 01:29:44 +0400

    Add project files.

---
+++ 14.06.2022  Asp-Intro/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _14._06._2022__Asp_Intro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
